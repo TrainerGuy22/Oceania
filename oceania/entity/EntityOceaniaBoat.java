2fcd76c4aed468e5ab1e7ba3277672e7d9862d50
TrainerGuy22 <rocketkid1@gmail.com>
2013-12-14 11:13:32 -0500

    Boats. YES.

---
+++ oceania/entity/EntityOceaniaBoat.java
@@ -0,0 +1,14 @@
+package oceania.entity;
+
+import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.world.World;
+
+public class EntityOceaniaBoat extends EntityBoat 
+{
+
+	public EntityOceaniaBoat(World world) 
+	{
+		super(world);
+	}
+
+}
---
+++ oceania/util/BoatTypes.java
@@ -0,0 +1,5 @@
+package oceania.util;
+
+public enum BoatTypes {
+
+}
