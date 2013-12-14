ac51c3459130a561f573b3dacde30250882a2a61
TrainerGuy22 <rocketkid1@gmail.com>
2013-12-13 20:16:26 -0500

    Atlantium! Stuff of legends!

--- oceania/items/ItemMulti.java
+++ oceania/items/ItemMulti.java
@@ -1,12 +1,17 @@
 package oceania.items;
 
+import java.util.List;
+
 import oceania.Oceania;
 import oceania.util.IconRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
 
 public class ItemMulti extends Item 
 {
@@ -17,12 +22,19 @@ public class ItemMulti extends Item
 	public ItemMulti(int par1) 
 	{
 		super(par1);
+		setCreativeTab(Oceania.CREATIVE_TAB);
 		this.hasSubtypes = true;
-		
 		for(int index = 0; index < ITEMS.length; index++) 
 		{
 			LanguageRegistry.instance().addStringLocalization("item.sub." + ITEMS[index] + "name", LANG_NAMES[index]);
 		}
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+    public String getUnlocalizedName(ItemStack stack) 
+	{
+		return "item.sub." + ITEMS[stack.getItemDamage()];
 	}
 	
 	@Override
@@ -34,5 +46,23 @@ public class ItemMulti extends Item
 			IconRegistry.setIcon(ITEMS[index], registry.registerIcon(Oceania.MOD_ID + ":" + ITEMS[index]));
 		}
 	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+    public Icon getIconFromDamage(int meta)
+	{
+		return IconRegistry.getIcon(ITEMS[meta]);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+    public void getSubItems(int ID, CreativeTabs tabs, List list) 
+	{
+		for(int index = 0; index < ITEMS.length; index++) 
+		{
+			list.add(new ItemStack(ID, 1, index));
+		}
+	}
+
 
 }
--- oceania/items/Items.java
+++ oceania/items/Items.java
@@ -3,6 +3,7 @@ package oceania.items;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraftforge.common.Configuration;
+import net.minecraftforge.oredict.OreDictionary;
 import oceania.Oceania;
 
 public class Items
@@ -10,6 +11,7 @@ public class Items
 	public static final int DEFAULT_ID_RANGE = 3750;
 	
 	public static ItemDivingHelmet itemDivingHelmet;
+	public static ItemMulti itemMulti;
 	
 	public static void initItems()
 	{
@@ -17,11 +19,16 @@ public class Items
 		
 		/** Set up instances */
 		itemDivingHelmet = new ItemDivingHelmet(cfg.getItem("itemDivingHelmet", DEFAULT_ID_RANGE).getInt(DEFAULT_ID_RANGE));
+		itemMulti = new ItemMulti(cfg.getItem("itemMulti", DEFAULT_ID_RANGE + 1).getInt(DEFAULT_ID_RANGE + 1));
 		
 		/** Add to registry */
-		GameRegistry.registerItem(itemDivingHelmet, "itemDivingHelmet");
+		GameRegistry.registerItem(itemDivingHelmet, Oceania.MOD_ID + "itemDivingHelmet");
+		GameRegistry.registerItem(itemMulti, Oceania.MOD_ID + "itemMulti");
+		
+		/** Ore Dictionary support */
+		OreDictionary.registerOre("oceaniaCraftingItems", itemMulti);
 		
 		/** Add language */
-		LanguageRegistry.addName(itemDivingHelmet, "Diving Helmet");
+		LanguageRegistry.addName(itemDivingHelmet, "Atlantium Helm");
 	}
 }
