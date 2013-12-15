1ce12265a85677e0bd513e703df272bbac9bb48d
TrainerGuy22 <rocketkid1@gmail.com>
2013-12-15 15:08:59 -0500

    Trade all the things!

--- oceania/entity/Entities.java
+++ oceania/entity/Entities.java
@@ -1,16 +1,27 @@
 package oceania.entity;
 
+import net.minecraft.util.ResourceLocation;
 import oceania.Oceania;
+import oceania.handler.VillageTradeHandler;
 import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.common.registry.VillagerRegistry;
 
 public class Entities 
 {
+		
+	public static int VILLAGER_OCEANIA_ID;
 	
 	public static void initEntities()
 	{
 		EntityRegistry.registerModEntity(EntityOceaniaBoatNormal.class, "OceaniaBoat", 0, Oceania.INSTANCE, 80, 3, true);
 		EntityRegistry.registerModEntity(EntityOceaniaBoatWithChest.class, "OceaniaBoatWithChest", 1, Oceania.INSTANCE, 80, 3, true);
 		EntityRegistry.registerModEntity(EntitySubmarine.class, "Submarine", 2, Oceania.INSTANCE, 80, 3, true);
+	
+		VILLAGER_OCEANIA_ID = VillagerRegistry.getRegisteredVillagers().size();
+		VillagerRegistry.instance().registerVillagerId(VILLAGER_OCEANIA_ID);
+		// TODO: Post ModJam - Have a custom skin for the villager.
+		VillagerRegistry.instance().registerVillagerSkin(VILLAGER_OCEANIA_ID, new ResourceLocation("textures/entity/villager/smith.png"));
+		VillagerRegistry.instance().registerVillageTradeHandler(VILLAGER_OCEANIA_ID, new VillageTradeHandler());
 	}
 	
 }
---
+++ oceania/handler/VillageTradeHandler.java
@@ -0,0 +1,18 @@
+package oceania.handler;
+
+import java.util.Random;
+
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.village.MerchantRecipeList;
+import cpw.mods.fml.common.registry.VillagerRegistry.IVillageTradeHandler;
+
+public class VillageTradeHandler implements IVillageTradeHandler
+{
+
+	@Override
+	public void manipulateTradesForVillager(EntityVillager villager, MerchantRecipeList list, Random random) 
+	{
+		
+	}
+
+}
