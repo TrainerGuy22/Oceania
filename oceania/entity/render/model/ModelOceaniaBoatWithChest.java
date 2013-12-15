7d802bd989c7035ca137aeade0e2c4e95d911d82
TrainerGuy22 <rocketkid1@gmail.com>
2013-12-14 23:21:49 -0500

    Working on texture scaling.

--- oceania/entity/render/model/ModelOceaniaBoatWithChest.java
+++ oceania/entity/render/model/ModelOceaniaBoatWithChest.java
@@ -1,11 +1,13 @@
 package oceania.entity.render.model;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import oceania.entity.EntityOceaniaBoatWithChest;
 import oceania.util.BoatTypes;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.AdvancedModelLoader;
@@ -17,13 +19,14 @@ public class ModelOceaniaBoatWithChest extends ModelBase
 	
 	public ModelOceaniaBoatWithChest()
 	{
-		model = (TechneModel) AdvancedModelLoader.loadModel("/assets/oceania/textures/models/boatWithChest.tcn");
+		model = ((TechneModel) AdvancedModelLoader.loadModel("/assets/oceania/textures/models/boatWithChest.tcn"));
 	}
 	
 	@Override
     public void render(Entity entity, float par2, float par3, float par4, float par5, float par6, float par7)
 	{
 		GL11.glScalef(par7, par7, par7);
+        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
 		EntityOceaniaBoatWithChest boat = (EntityOceaniaBoatWithChest) entity;
 		try 
 		{
