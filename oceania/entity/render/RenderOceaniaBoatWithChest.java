b4e007d4f590bd30fe69571ba6d7b7b472ffea64
TrainerGuy22 <rocketkid1@gmail.com>
2013-12-14 20:09:15 -0500

    The little things. Been researching.

--- oceania/entity/render/RenderOceaniaBoatWithChest.java
+++ oceania/entity/render/RenderOceaniaBoatWithChest.java
@@ -1,6 +1,16 @@
 package oceania.entity.render;
 
-public class RenderOceaniaBoatWithChest 
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderBoat;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderOceaniaBoatWithChest extends RenderBoat
 {
+
+	@Override
+	protected ResourceLocation getEntityTexture(Entity entity) {
+		return null;
+	}
 
 }
